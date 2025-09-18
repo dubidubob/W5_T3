aac05961566b940142a7fa5762bd334ed9690ad1
Donghee <luz0415@naver.com>
2025-09-18 21:06:16 +0900

    Add | Vector2D 추가

--- Engine/Global/Vector.cpp
+++ Engine/Global/Vector.cpp
@@ -210,3 +210,68 @@ const FVector FVector::OneVector = FVector(1.0f, 1.0f, 1.0f);
 const FVector4 FVector4::ZeroVector = FVector4(0.0f, 0.0f, 0.0f, 0.0f);
 const FVector4 FVector4::OneVector = FVector4(1.0f, 1.0f, 1.0f, 1.0f);
 
+// FVector2D static const 멤버 변수 정의
+const FVector2D FVector2D::ZeroVector = FVector2D(0.0f, 0.0f);
+const FVector2D FVector2D::OneVector = FVector2D(1.0f, 1.0f);
+const FVector2D FVector2D::UnitX = FVector2D(1.0f, 0.0f);
+const FVector2D FVector2D::UnitY = FVector2D(0.0f, 1.0f);
+
+FVector2D::FVector2D()
+{
+	X = 0.0f;
+	Y = 0.0f;
+}
+
+FVector2D::FVector2D(float InX, float InY)
+{
+	X = InX;
+	Y = InY;
+}
+
+FVector2D::FVector2D(const FVector2D& InOther)
+{
+	X = InOther.X;
+	Y = InOther.Y;
+}
+
+FVector2D::FVector2D(const FVector& InOther)
+{
+	X = InOther.X;
+	Y = InOther.Y;
+}
+
+FVector2D FVector2D::operator+(const FVector2D& InOther) const
+{
+	return FVector2D(X + InOther.X, Y + InOther.Y);
+}
+
+FVector2D FVector2D::operator-(const FVector2D& InOther) const
+{
+	return FVector2D(X - InOther.X, Y - InOther.Y);
+}
+
+FVector2D FVector2D::operator*(const float Ratio) const
+{
+	return FVector2D(X * Ratio, Y * Ratio);
+}
+
+FVector2D& FVector2D::operator+=(const FVector2D& InOther)
+{
+	X += InOther.X;
+	Y += InOther.Y;
+	return *this;
+}
+
+FVector2D& FVector2D::operator-=(const FVector2D& InOther)
+{
+	X -= InOther.X;
+	Y -= InOther.Y;
+	return *this;
+}
+
+FVector2D& FVector2D::operator*=(const float Ratio)
+{
+	X *= Ratio;
+	Y *= Ratio;
+	return *this;
+}
