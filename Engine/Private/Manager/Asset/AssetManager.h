24b829aed2a9093e951735b18fa3e6837e30b7ac
yunsoo <dev.yunsoo@gmail.com>
2025-09-19 22:24:13 +0900

    feat: AssetManager

---
+++ Engine/Private/Manager/Asset/AssetManager.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Core/Object.h"
+class UAssetManager : public UObject
+{
+	DECLARE_SINGLETON(UAssetManager)
+
+public:
+
+};
---
+++ Engine/Public/Manager/Asset/AssetManager.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "Manager/Asset/AssetManager.h"
+
+
+IMPLEMENT_SINGLETON(UAssetManager)
+
+UAssetManager::UAssetManager()
+{
+
+}
+
+UAssetManager::~UAssetManager()
+{
+
+}
+
+
