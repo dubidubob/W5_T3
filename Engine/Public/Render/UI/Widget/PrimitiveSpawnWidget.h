4991285d2b5b9d43a0d5334147c6ba0b12e59de3
yunsoo <dev.yunsoo@gmail.com>
2025-09-23 20:21:36 +0900

    feat: 주석 추가

--- Engine/Public/Render/UI/Widget/PrimitiveSpawnWidget.h
+++ Engine/Public/Render/UI/Widget/PrimitiveSpawnWidget.h
@@ -5,14 +5,7 @@ class UPrimitiveSpawnWidget : public UWidget
 {
 	DECLARE_CLASS(UPrimitiveSpawnWidget, UWidget)
 public:
-	//inline static const char* PrimitiveTypes[] = {
-	//"Cube",
-	//"Sphere",
-	//"Triangle",
-	//"Square",
-	//"Demon",
-	//"Car"
-	//};
+	// 새로운 OBJ 파일은 여기에 이름만 써주면 된다.
 	static inline TArray<const char*> PrimitiveTypes { "Cube" ,"Sphere" ,"Cone" ,"Cylinder","Torus" ,"Demon" ,"Car"};
 	void Initialize() override;
 	void Update() override;
